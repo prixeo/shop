72f2e1e54bc4c26816b133c07cba5d5429521dcf
Harald-86 <79138134+Harald-86@users.noreply.github.com>
2024-04-12 10:49:08 +0200

    Change of Brand
    Replaced Medusa with Prixeo on the following components:
    - Nav
    - Footer

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -3,14 +3,14 @@ import { Button, Heading } from "@medusajs/ui"
 
 const Hero = () => {
   return (
-    <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
+    <div className="h-[50vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
       <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
         <span>
           <Heading
             level="h1"
             className="text-3xl leading-10 text-ui-fg-base font-normal"
           >
-            Ecommerce Starter Template
+            Prixeo.com
           </Heading>
           <Heading
             level="h2"
@@ -19,15 +19,6 @@ const Hero = () => {
             Powered by Medusa and Next.js
           </Heading>
         </span>
-        <a
-          href="https://github.com/medusajs/nextjs-starter-medusa"
-          target="_blank"
-        >
-          <Button variant="secondary">
-            View on GitHub
-            <Github />
-          </Button>
-        </a>
       </div>
     </div>
   )
